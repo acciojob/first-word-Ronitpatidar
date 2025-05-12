9f2e6435d7a615fb58fab8a0314d495e0963aeef
Ronitpatidar <ronitpatidar22@gmail.com>
2025-05-12 10:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
-  // your code here
+  // Trim any leading or trailing whitespace
+  let trimmedStr = s.trim();
+
+  // If the string is empty after trimming, return an empty string
+  if (trimmedStr === '') return '';
+
+  // Find the index of the first space
+  const spaceIndex = trimmedStr.indexOf(' ');
+
+  // If there is no space, return the entire string
+  if (spaceIndex === -1) {
+    return trimmedStr;
+  }
+
+  // Otherwise, return the substring from the start to the first space
+  return trimmedStr.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
